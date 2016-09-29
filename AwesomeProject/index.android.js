b920ebac62d7ad9ee5a6c6e9ead528e30b7996c1
陶发齐 <faqi.tao@gewara.com>
2016-09-29 15:06:21 +0800

    1.State（状态）

--- AwesomeProject/index.android.js
+++ AwesomeProject/index.android.js
@@ -3,33 +3,67 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
 import React, { Component } from 'react';
 import { AppRegistry,StyleSheet,Text,View,Image } from 'react-native';
 
-class Greeting extends Component{
-	render(){
-		return(
-			<Text> Hello{this.props.name}!</Text>
-		);
-	}
+class Greeting extends Component {
+    render() {
+        return (
+            <Text> Hello{this.props.name}!</Text>
+        );
+    }
 }
 
 class AwesomeProject extends Component {
-  render() {
-  	
-  	// let pic = {
-  	// 	uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-  // };
+    render() {
 
-    return (
-      // <Image source={pic} style={{width :193,height:110}}/>
-      <View style={{alignItems:'center'}}>
-      	<Greeting name="Rexxar"/>
-      	<Greeting name="Tom"/>
-      	<Greeting name="Jerry"/>
-      </View>
-    );
-  }
+        // let pic = {
+        // 	uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+        // };
+
+        return (
+            // <Image source={pic} style={{width :193,height:110}}/>
+            <View style={{alignItems:'center'}}>
+                <Greeting name="Rexxar"/>
+                <Greeting name="Tom"/>
+                <Greeting name="Jerry"/>
+            </View>
+        );
+    }
 }
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+
+
+class Blink extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {showText: true};
+
+        // 每1000毫秒对showText状态做一次取反操作
+        setInterval(() => {
+            this.setState({showText: !this.state.showText});
+        }, 1000);
+    }
+
+    render() {
+        // 根据当前showText的值决定是否显示text内容
+        let display = this.state.showText ? this.props.text : ' ';
+        return (
+            <Text>{display}</Text>
+        );
+    }
+}
+
+class BlinkApp extends Component {
+    render() {
+        return (
+            <View>
+                <Blink text='I love to blink'/>
+                <Blink text='Yes blinking is so great'/>
+                <Blink text='Why did they ever take this out of HTML'/>
+                <Blink text='Look at me look at me look at me'/>
+            </View>
+        );
+    }
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => BlinkApp);
